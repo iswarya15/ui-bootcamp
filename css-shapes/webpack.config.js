cff52a9b802fe7283c8dfe10635460169793f6ae
iswarya <iswarya.ch99@gmail.com>
2023-01-31 12:25:34 +0530

    challenge: css shapes

---
+++ css-shapes/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    index: "./src/index.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+
+  devServer: {
+    hot: true,
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    watchFiles: ["src/*.html"],
+  },
+};
